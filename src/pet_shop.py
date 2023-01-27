7bd5e283fed70fe9e8ae722e3040aeddc44c7bbc
Chris Barclay <chris.barclay1@gmail.com>
2023-01-27 13:57:02 +0000

    Test 3 and 4 completed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -4,6 +4,10 @@
 def get_pet_shop_name(pet_shop_details):
     return pet_shop_details["name"]
 
+# Test 2 - Total Cash
 def get_total_cash(pet_shop_details):
     return pet_shop_details["admin"]["total_cash"]
-    
+
+# # Test 3 - Add or Remove Cash
+def add_or_remove_cash(pet_shop_details, cash):
+    pet_shop_details["admin"]["total_cash"] += cash
